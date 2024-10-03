9cfb3f23dd761063877d69f34589c6f0cef5aa98
raziyeyolasigmayoglu <raziyeaka89@gmail.com>
2024-10-03 14:42:11 +0100

    improvement UI

---
+++ wie/Core/Components/CustomButtonStyle.swift
@@ -0,0 +1,18 @@
+//
+//  CustomButtonStyle.swift
+//  wie
+//
+//  Created by raziye yolasigmazoglu on 03/10/2024.
+//
+
+import SwiftUI
+
+struct CustomButtonStyle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomButtonStyle()
+}
